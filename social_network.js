de3ed5d587015ee56dac83351bce5cbe66f9f06a
Waqas Bajwa <waqasbaj@gmail.com>
2018-01-17 23:07:26 +0000

    finished 5 out of 7 functions.

--- social_network.js
+++ social_network.js
@@ -114,7 +114,99 @@ function whoisfollowed()
 
 }
 
-whoisfollowed();
+
+
+function whoisfollowedover30()
+{
+  var obj={};
+
+  for(word in data)
+  {
+    if(data[word].age >30)
+    {
+      for(x=0; x<data[word].follows.length; x++)
+      {
+
+
+        if(obj[data[word].follows[x]] === undefined)
+        {
+          obj[data[word].follows[x]] = 1;
+        }
+        else
+        {
+          obj[data[word].follows[x]]++;
+        }
+
+      }
+    }
+
+  }
+
+  var y = "";
+  var z = 0;
+
+  for (word in obj)
+  {
+     var x = obj[word];
+     {
+      if(x > z)
+      {
+        y = word;
+        z = x;
+      }
+     }
+  }
+
+  console.log(data[y].name +" has most followers over 30");
+
+}
+
+
+
+function whofollowsmostover30()
+{
+  var y = "";
+  var z = 0;
+
+  for (word in data)
+  {
+     var x = data[word].follows.length;
+     {
+
+      for(g=0; g<data[word].follows.length; g++)
+      {
+        if(data[data[word].follows[g]].age < 30)
+        {
+          x--;
+        }
+      }
+
+
+        if(x > z)
+        {
+        y = data[word].name;
+        z = x;
+      }
+     }
+  }
+
+  console.log(y +" follows most people over 30");
+
+}
+
+whofollowsmostover30();
+
+
+// List those who follow someone that doesn't follow them back
+
+//   initialise an empty array
+//   make a loop of for (x in y) to iterate through the keys of data
+//   make a subloop of to compare the
+
+
+
+
+
 
 
 
